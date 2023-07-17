9c9b04e758632384920c9b745553956c31bf4595
Huy Du <dugiahuy@gmail.com>
2023-07-18 00:21:52 +0800

    Add install.sh Prezto

--- install.sh
+++ install.sh
@@ -5,13 +5,22 @@ sudo xcodebuild -license # agree with license
 xcode-select --install # Install command line tool
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
 brew bundle --file=./Brewfile
 
 # Config github
 git config --global user.name dugiahuy
 git config --global user.email dugiahuy@gmail.com
+
+# Install Prezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# Set ZSH as default shall
+chsh -s /bin/zsh
 
 # Make symlinks
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
