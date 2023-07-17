ac81bcceabf77258958b6a26bddc85c44fd2060b
Huy Du <dugiahuy@gmail.com>
2023-07-18 00:52:01 +0800

    Update install.sh with echo

--- install.sh
+++ install.sh
@@ -4,24 +4,40 @@
 sudo xcodebuild -license # agree with license
 xcode-select --install # Install command line tool
 
+echo "Install HomeBrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+echo "Install Brewfile"
 brew bundle --file=./Brewfile
 
 # Config github
+echo "Config Git"
 git config --global user.name dugiahuy
 git config --global user.email dugiahuy@gmail.com
 
 # Install Prezto
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
+if [[ -d "~/.zprezto" ]]; then
+  echo "Prezto installed, refresh configs"
+  setopt EXTENDED_GLOB
+  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/*.md; do
+    if [[ "$rcfile" != *"README.md" ]]; then
+      ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile##*/}"
+    fi
+  done
+else
+  echo "Install Prezto"
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+  setopt EXTENDED_GLOB
+  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/*.md; do
+    if [[ "$rcfile" != *"README.md" ]]; then
+      ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile##*/}"
+    fi
+  done
+fi
 
-# Set ZSH as default shall
+echo "Set ZSH as default shell"
 chsh -s /bin/zsh
 
-# Make symlinks
+echo "Make symlinks to .zshrc and .zpreztorc"
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
